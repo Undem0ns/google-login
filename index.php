1c397c986cdfd5acf96b871301c96d2c28ce7476
andamannr <andaman@super6group.com>
2022-10-11 10:52:30 +0700

    Success

--- index.php
+++ index.php
@@ -9,22 +9,66 @@
 
 <body>
     <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
-    <script>
-        function onSignIn(googleUser) {
-            console.log(123)
-            // Useful data for your client-side scripts:
-            var profile = googleUser.getBasicProfile();
-            console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-            console.log('Full Name: ' + profile.getName());
-            console.log('Given Name: ' + profile.getGivenName());
-            console.log('Family Name: ' + profile.getFamilyName());
-            console.log("Image URL: " + profile.getImageUrl());
-            console.log("Email: " + profile.getEmail());
+    <button id="gp-login-btn">Sign in with Google</button>
+    <br>
+    Result: <textarea id="result"></textarea>
 
-            // The ID token you need to pass to your backend:
-            var id_token = googleUser.getAuthResponse().id_token;
-            console.log("ID Token: " + id_token);
-        }
+    <script>
+    function onSignIn(googleUser) {
+        console.log(123)
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+    }
+
+    function bindGpLoginBtn() {
+        gapi.load('auth2', function() {
+            auth2 = gapi.auth2.init({
+                client_id: 'YOUR_CLIENT_ID',
+                scope: 'profile email'
+            });
+            attachSignin(document.getElementById('gp-login-btn'));
+        });
+    }
+
+    function attachSignin(element) {
+        auth2.attachClickHandler(element, {},
+            function(googleUser) {
+                console.log('Success')
+                // Success
+                getCurrentGpUserInfo(googleUser);
+            },
+            function(error) {
+                // Error
+                console.log(JSON.stringify(error, undefined, 2));
+            }
+        );
+    }
+
+    function getCurrentGpUserInfo(userInfo) {
+        var result = '';
+
+        // Useful data for your client-side scripts:
+        var profile = userInfo.getBasicProfile();
+
+        result += "ID: " + profile.getId() + "\n";
+        result += "Full Name:  " + profile.getName() + "\n";
+        result += "Given Name: " + profile.getGivenName() + "\n";
+        result += "Family Name: " + profile.getFamilyName() + "\n";
+        result += "Email: " + profile.getEmail() + "\n";
+        result += "ID Token: " + userInfo.getAuthResponse().id_token + "\n";
+
+        document.getElementById("result").value = result;
+    }
     </script>
 </body>
 
