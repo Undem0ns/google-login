778b0d944a0f2b85ff56a48791d03672077777bf
andamannr <andaman@super6group.com>
2022-10-11 11:46:24 +0700

    asdasdasd

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <html lang="en">
 
+<header>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+</header>
+
 <body>
     <script src="https://accounts.google.com/gsi/client" async defer></script>
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <div id="g_id_onload" data-client_id="927679325793-nr98rvaq74vjvipp6gl5b3g9h4jeo7g4" data-callback="onSignIn">
     </div>
     <div class="g_id_signin" data-type="standard"></div>
 
-    <script>
+    <script setup>
     const axios = require('axios').default;
 
     function onSignIn(googleUser) {
